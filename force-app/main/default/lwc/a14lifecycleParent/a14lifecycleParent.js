c7fe23bcf90bbf3e02cecc5b3b417ad03f4dbc3c
Mehmet <memet-akgul@windowslive.com>
2024-03-19 12:40:46 -0700

    0319

--- force-app/main/default/lwc/a14lifecycleParent/a14lifecycleParent.js
+++ force-app/main/default/lwc/a14lifecycleParent/a14lifecycleParent.js
@@ -4,42 +4,83 @@ import fetchAccounts from '@salesforce/apex/AccountController.fetchAccounts'
 
 
 export default class A14lifecycleParent extends LightningElement {
-name
+  name
 handleName(event){
     this.name= event.target. value
     console.log('name >> ' + this.name)
 }
 
 
+flag=true
+
+handleGoster(){
+this.flag=true
+}
+handleGizle(){
+  this.flag= false
+}
+
     constructor(){
         super()
-        
+    
         fetchAccounts()
-        .then((result)=> {console.log('Constructor>>> ' + JSON.stringify(result))})
-        .catch( )
+        .then( (result) => {'constructor >>>> '+console.log( JSON.stringify(result)     )}   )
+        .catch(  (error) => {console.log( JSON.stringify(error)     )}     )
 
+
+
+     //alert(' PARENT   constructor  calisti ')
     }
  
-
 connectedCallback(){
- 
-    fetchAccounts()
-    .then((result)=> {console.log('Constructor>>> ' + JSON.stringify(result))})
-    .catch( (error)=> {console.log('Constructor>>> ' + JSON.stringify(error))})
 
+
+  fetchAccounts()
+  .then( (result) => {console.log('connectedCallback >>>> ' + JSON.stringify(result)     )}   )
+  .catch(  (error) => {console.log( JSON.stringify(error)     )}     )
+
+ // alert('PARENT   connectedCallback  calisti ')
+ 
 }
 
 
+sayi=0
+
 
 renderedCallback(){
-   
+/*
+  if ( this.sayi<5){
+    this.sayi++
+  }
+  
 
+  alert('PARENT  renderedCallback  calisti ' + this.sayi)
+*/
 }
 
 
 disconnectedCallback(){
 
+ // alert('PARENT  disconnectedCallback  calisti ')
+
 
 }
+
+hata=true
+errorCallback( error, stack    ){
+  if(error){
+    this.hata=false
+  }
+
+
+/*
+if(error){
+  console.log('ERROR OLUSTU ' + error)
+} 
+*/
+//console.log('stack ' + stack)
+
+}
+
 
 }
